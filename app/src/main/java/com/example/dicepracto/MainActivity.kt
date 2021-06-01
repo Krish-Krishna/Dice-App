e8e7ef49e0f4a1032a5af37f9bf8c57f38d4bf9b
Sai Krishna <disneykrishna@gmail.com>
2021-06-01 17:47:13 +0530

    Dice app

---
+++ app/src/main/java/com/example/dicepracto/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.dicepracto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var image:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener { roll() }
+        image = findViewById(R.id.diceImage)
+    }
+
+    private fun roll(){
+        val showImage =when(Random.nextInt(0,7)) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        image.setImageResource(showImage)
+    }
+}
